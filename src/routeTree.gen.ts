0751ea7173062ef36a3297fbfd858d60a411ea42
Praveen Garimella <praveeng@andrew.cmu.edu>
2026-09-04 15:12:40 +0530

    fix: update route tree to include advisory route

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -11,6 +11,7 @@
 import { Route as rootRouteImport } from './routes/__root'
 import { Route as IndexRouteImport } from './routes/index'
 import { Route as AboutRouteImport } from './routes/about'
+import { Route as AdvisoryRouteImport } from './routes/advisory'
 import { Route as ContactRouteImport } from './routes/contact'
 import { Route as JoinRouteImport } from './routes/join'
 import { Route as NewsRouteImport } from './routes/news'
@@ -31,6 +32,11 @@ const IndexRoute = IndexRouteImport.update({
 const AboutRoute = AboutRouteImport.update({
   id: '/about',
   path: '/about',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const AdvisoryRoute = AdvisoryRouteImport.update({
+  id: '/advisory',
+  path: '/advisory',
   getParentRoute: () => rootRouteImport,
 } as any)
 const ContactRoute = ContactRouteImport.update({
@@ -92,6 +98,7 @@ const ResearchThirdAgeRoute = ResearchThirdAgeRouteImport.update({
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
   '/about': typeof AboutRoute
+  '/advisory': typeof AdvisoryRoute
   '/contact': typeof ContactRoute
   '/join': typeof JoinRoute
   '/news': typeof NewsRoute
@@ -107,6 +114,7 @@ export interface FileRoutesByFullPath {
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
   '/about': typeof AboutRoute
+  '/advisory': typeof AdvisoryRoute
   '/contact': typeof ContactRoute
   '/join': typeof JoinRoute
   '/news': typeof NewsRoute
@@ -122,6 +130,7 @@ export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
   '/about': typeof AboutRoute
+  '/advisory': typeof AdvisoryRoute
   '/contact': typeof ContactRoute
   '/join': typeof JoinRoute
   '/news': typeof NewsRoute
@@ -139,6 +148,7 @@ export interface FileRouteTypes {
   fullPaths:
     | '/'
     | '/about'
+    | '/advisory'
     | '/contact'
     | '/join'
     | '/news'
@@ -154,6 +164,7 @@ export interface FileRouteTypes {
   to:
     | '/'
     | '/about'
+    | '/advisory'
     | '/contact'
     | '/join'
     | '/news'
@@ -168,6 +179,7 @@ export interface FileRouteTypes {
     | '__root__'
     | '/'
     | '/about'
+    | '/advisory'
     | '/contact'
     | '/join'
     | '/news'
@@ -184,6 +196,7 @@ export interface FileRouteTypes {
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
   AboutRoute: typeof AboutRoute
+  AdvisoryRoute: typeof AdvisoryRoute
   ContactRoute: typeof ContactRoute
   JoinRoute: typeof JoinRoute
   NewsRoute: typeof NewsRoute
@@ -206,6 +219,13 @@ declare module '@tanstack/react-router' {
       path: '/about'
       fullPath: '/about'
       preLoaderRoute: typeof AboutRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/advisory': {
+      id: '/advisory'
+      path: '/advisory'
+      fullPath: '/advisory'
+      preLoaderRoute: typeof AdvisoryRouteImport
       parentRoute: typeof rootRouteImport
     }
     '/contact': {
@@ -311,6 +331,7 @@ const ResearchRouteWithChildren = ResearchRoute._addFileChildren(
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
   AboutRoute: AboutRoute,
+  AdvisoryRoute: AdvisoryRoute,
   ContactRoute: ContactRoute,
   JoinRoute: JoinRoute,
   NewsRoute: NewsRoute,
