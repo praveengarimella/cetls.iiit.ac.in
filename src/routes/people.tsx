49b9062e78047885deb1f027da22aed6823e2eb3
Praveen Garimella <praveeng@andrew.cmu.edu>
2026-09-04 17:16:02 +0530

    feat: add Bapi Raju picture to affiliate faculty

--- src/routes/people.tsx
+++ src/routes/people.tsx
@@ -44,7 +44,7 @@ const adjunctFaculty = [
 
 const affiliateFaculty = [
   { name: "Kavita Vemuri", roles: ["Affiliate Faculty"], photo: "https://faculty.iiit.ac.in/~kvemuri/images/team/kavita.jpg", description: "", profileUrl: "" },
-  { name: "Bapi Raju", roles: ["Affiliate Faculty"], photo: "https://ui-avatars.com/api/?name=Bapi+Raju&background=0D8ABC&color=fff&size=150", description: "", profileUrl: "" },
+  { name: "Bapi Raju", roles: ["Affiliate Faculty"], photo: "/people/bapi-raju.png", description: "", profileUrl: "" },
   { name: "Vishnu Sreekumar", roles: ["Affiliate Faculty"], photo: "https://ui-avatars.com/api/?name=Vishnu+Sreekumar&background=0D8ABC&color=fff&size=150", description: "", profileUrl: "" },
   { name: "Bhaktee Dongaonkar", roles: ["Affiliate Faculty"], photo: "https://ui-avatars.com/api/?name=Bhaktee+Dongaonkar&background=0D8ABC&color=fff&size=150", description: "", profileUrl: "" },
   { name: "Prof C Venkatesh", roles: ["Affiliate Faculty"], photo: "https://ui-avatars.com/api/?name=Prof+C+Venkatesh&background=0D8ABC&color=fff&size=150", description: "", profileUrl: "" },
