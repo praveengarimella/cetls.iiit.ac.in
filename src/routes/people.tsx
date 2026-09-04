70cf5e1dc69dbebc8136afa6d28a0b4c1f1ab1d7
Praveen Garimella <praveeng@andrew.cmu.edu>
2026-09-04 17:17:45 +0530

    feat: add Vishnu Sreekumar picture to affiliate faculty

--- src/routes/people.tsx
+++ src/routes/people.tsx
@@ -45,7 +45,7 @@ const adjunctFaculty = [
 const affiliateFaculty = [
   { name: "Kavita Vemuri", roles: ["Affiliate Faculty"], photo: "https://faculty.iiit.ac.in/~kvemuri/images/team/kavita.jpg", description: "", profileUrl: "" },
   { name: "Bapi Raju", roles: ["Affiliate Faculty"], photo: "/people/bapi-raju.png", description: "", profileUrl: "" },
-  { name: "Vishnu Sreekumar", roles: ["Affiliate Faculty"], photo: "https://ui-avatars.com/api/?name=Vishnu+Sreekumar&background=0D8ABC&color=fff&size=150", description: "", profileUrl: "" },
+  { name: "Vishnu Sreekumar", roles: ["Affiliate Faculty"], photo: "/people/vishnu-sreekumar.png", description: "", profileUrl: "" },
   { name: "Bhaktee Dongaonkar", roles: ["Affiliate Faculty"], photo: "/people/bhaktee-dongaonkar.png", description: "", profileUrl: "" },
   { name: "Prof C Venkatesh", roles: ["Affiliate Faculty"], photo: "https://ui-avatars.com/api/?name=Prof+C+Venkatesh&background=0D8ABC&color=fff&size=150", description: "", profileUrl: "" },
   { name: "Soma Paul", roles: ["Affiliate Faculty"], photo: "https://ui-avatars.com/api/?name=Soma+Paul&background=0D8ABC&color=fff&size=150", description: "", profileUrl: "" },
